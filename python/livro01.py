ca6e038e532b689d4f9ecdd78d3652ba00aba505
malow <134291030+MTSmalow@users.noreply.github.com>
2024-01-25 21:37:33 -0300

    Add files via upload

---
+++ python/livro01.py
@@ -0,0 +1,5 @@
+s = float(input("quanto voce recebe de salario? "))
+if s > 1200.00:
+    print("voce deve pagar imposto")
+else:
+    print("voce nao precissa pagra imposto")
---
+++ python/livro02.py
@@ -0,0 +1,8 @@
+from calendar import*
+year  = int(input(" coloque o ano: "))
+print(calendar(year, 2, 1, 8, 3))
+
+# 2 = 2 caracteres para dias 
+# 1 = 1 linha para cada semana
+# 8 = 8 linhas para cada mes
+# 3 = 3 colunas para todos os meses do ano
