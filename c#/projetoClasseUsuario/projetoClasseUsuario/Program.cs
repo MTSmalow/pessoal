f83fa81c08dc16e1864ad161dcb173dcadf52648
malow <134291030+MTSmalow@users.noreply.github.com>
2024-03-01 19:10:53 -0300

    Add files via upload

---
+++ c#/projetoClasseUsuario/projetoClasseUsuario/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projetoClasseUsuario
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Usuario fulano;
+            fulano = new Usuario();
+
+
+            Usuario beltrano;
+            beltrano = fulano;
+
+            fulano.Nome = "fulano de tal";
+            beltrano.Idade= 18;
+            beltrano.Email = "pepeu@hotlady.com";
+
+            Console.WriteLine(fulano);
+            Console.ReadKey();  
+        }
+    }
+}
---
+++ c#/projetoClasseUsuario/projetoClasseUsuario/Usuario.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projetoClasseUsuario
+{
+    public class Usuario
+    {
+        string nome;
+        int idade;
+        string endereço;
+        string email;
+
+        public string Nome 
+        { 
+            get
+            {
+                return nome;
+            }
+            set
+            {
+                if(value.Length > 5) 
+                {
+                    nome = value.ToUpper();
+                }
+                else
+                {
+                    throw new Exception("Nome invalido");
+                }
+            }
+                
+        }
+
+        public int Idade { get => idade; set => idade = value; } // arrowFunctiom , e igual a de cima mais de forma simplificada
+        public string Endereço { get => endereço; set => endereço = value; }
+        public string Email { get => email; set => email = value; }
+
+        public override string ToString()
+        {
+            return nome+ " "+ email+ " "+ idade;
+        }
+    }
+}
